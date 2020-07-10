5da5af3390419d953c30be486e3b1806ee1fed96
asasankreddy <asasankr@gmail.com>
2020-07-10 14:26:49 +0530

    Updates

---
+++ precache-manifest.9c447f5d0548a19de2950c7df58134e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35a91d2d749a06edfca2dfc6e9f3b09f",
+    "url": "/reactapp/index.html"
+  },
+  {
+    "revision": "c54ef6a5a62d4b25910a",
+    "url": "/reactapp/static/css/main.8a18c7c4.chunk.css"
+  },
+  {
+    "revision": "9937691bd6eeaee43597",
+    "url": "/reactapp/static/js/2.2634b53c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactapp/static/js/2.2634b53c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c54ef6a5a62d4b25910a",
+    "url": "/reactapp/static/js/main.9a732393.chunk.js"
+  },
+  {
+    "revision": "e219a2030a13ac34f0be",
+    "url": "/reactapp/static/js/runtime-main.f935dd26.js"
+  }
+]);
